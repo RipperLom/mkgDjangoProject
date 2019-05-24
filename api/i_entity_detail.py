4fb1234c3c4739df54717b96c4add03213f27636
gaofei <1010155688@qq.com>
2019-05-24 19:59:58 +0800

    gaofei v0.1_9

--- api/i_entity_detail.py
+++ api/i_entity_detail.py
@@ -10,7 +10,7 @@ class EntityDetailyApi(BaseApi):
         # send data to Invoker
         # in: entity : str
         # out: （1） 如果实体不存在，返回{error:'实体不存在'}
-        #       (2)  如果实体存在，返回{name: '', detail: '', little_propreties: ['key':{'key': 'content'},...], large_propreties: ['key':{'key': 'content'},...] , error: ''}
-        entity = kwargs.get('entity')
+        #       (2)  如果实体存在，返回{name: '', detail: '', url: '', 'little_propreties:' ['key':{'key': 'content'},...], large_propreties: ['key':{'key': 'content'},...] , error: ''}
+        entity = kwargs.get('entity', '')
 
         return self.result
